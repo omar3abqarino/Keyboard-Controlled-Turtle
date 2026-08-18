d1577af32cf701dbad621e546ce688035cb4113a
Omar Samy <omar3abqarino@gmail.com>
2026-08-18 17:44:30 +0300

    Some edits to the Movement Implemented Perception

--- src/my_pkg/my_pkg/move_perception.py
+++ src/my_pkg/my_pkg/move_perception.py
@@ -1,9 +1,12 @@
 import rclpy
-from sshkeyboard import listen_keyboard, stop_listening
+import threading
+from sshkeyboard import listen_keyboard, stop_listening, listen_keyboard_manual
+from std_msgs.msg import String
 import sys
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 from geometry_msgs.msg import Twist, TwistStamped
+from turtlesim.msg import Color
 
 MOVE_BINDINGS = {
     'w': (2.0, 0.0),    # Forward
@@ -17,6 +20,7 @@ class Move_PerceptionNode(Node):
         super().__init__("move_perception_node")
 
         self.use_stamped_vel = False
+        self.latest_dominant_ch = "X"
 
         self.get_logger().info(
             "\nControl Your Turtle!\n"
@@ -37,20 +41,30 @@ class Move_PerceptionNode(Node):
         else:
             #self.keys_subscriber = self.create_subscription(Twist, "/cmd_vel", self.keys_publish, 10)
 
-            self.keys_publisher = self.create_publisher(Twist, "/cmd_vel", 10)
-        
+            self.keys_publisher = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
 
+        self.color_subscriber = self.create_subscription(Color, "/turtle1/color_sensor", self.color_subscribe, 10)
+        self.color_publisher = self.create_publisher(String, "/dominant_color", 10)
         
-
-    # def press(self, key):
-    #     print(f"'{key}' pressed")
-    #     stop_listening()
-    #     return key
+        #self.keys_publish(listen_keyboard_manual().get_next_key())
     
-    def key_capture(self):
-        listen_keyboard(on_press=self.keys_publish)
-        
 
+    def color_subscribe(self, msg):
+        red  = msg.r
+        green = msg.g
+        blue = msg.b
+
+        if red == green == blue:
+            self.latest_dominant_ch = "White"
+        elif red > green and red > blue:
+            self.latest_dominant_ch = "Red"
+        elif green > red and green > blue:
+            self.latest_dominant_ch = "Green"
+        else:
+            self.latest_dominant_ch = "Blue"
+
+        
+        
 
     def keys_publish(self, key):
         if not rclpy.ok():
@@ -65,15 +79,24 @@ class Move_PerceptionNode(Node):
             msg.angular.z = angular_move
 
             self.keys_publisher.publish(msg)
-            self.get_logger().info(f'Forwarded standard Twist: Linear X={msg.linear.x}')
-            self.get_logger().info(f'Forwarded standard Twist: Angular Z={msg.angular.z}')
+            self.get_logger().info(f'Standard Twist: Linear X = {msg.linear.x}')
+            self.get_logger().info(f'Standard Twist: Angular Z = {msg.angular.z}')
+
+            self.get_logger().info(f"The Dominant Color is {self.latest_dominant_ch}")
+            
+            dom_color = String()
+            dom_color.data = self.latest_dominant_ch[0].lower()
+            
+            self.color_publisher.publish(dom_color)
+
         elif key == "q":
             self.get_logger().info("Exit key pressed. Shutting down...")
             stop_listening()
             stop_msg = Twist()
             self.keys_publisher.publish(stop_msg)
             rclpy.shutdown()
-            #sys.exit(0)
+
+
 
     def stamped_keys_publish(self, msg):
         actual_msg = TwistStamped()
@@ -94,11 +117,25 @@ class Move_PerceptionNode(Node):
 
 
 def main():
+    print("1. Stamped Twist")
+    print("2. Normal Twist")
+    while True:
+        use_stamped_vel = input("Choose: ").strip()
+        if use_stamped_vel.isdecimal() and use_stamped_vel in ["1", "2"]:
+            use_stamped_vel = int(use_stamped_vel)
+            break
+        else:
+            continue
     rclpy.init()
+    #add use_stamped_vel
     node  = Move_PerceptionNode()
 
     try:
+    
+        spin_thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
+        spin_thread.start()
         listen_keyboard(on_press=node.keys_publish)
+            
     except (KeyboardInterrupt, ExternalShutdownException):
         pass
     finally:
